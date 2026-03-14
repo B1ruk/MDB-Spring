7e17cbf4a8e76ea8f3bd7adb5414241fda02738b
b1ruk <biruk3870@gmail.com>
2026-03-14 05:06:41 +0300

    implement user collection storage

--- src/main/java/io/java/remove/IMDB_MDS/model/User.java
+++ src/main/java/io/java/remove/IMDB_MDS/model/User.java
@@ -1,4 +1,4 @@
 package io.java.remove.IMDB_MDS.model;
 
-public record User(String name, String apiKey,Subscription subscription) {
+public record User(String name, String apiKey,String email,Subscription subscription) {
 }
--- src/main/java/io/java/remove/IMDB_MDS/subscription/SubscriptionManagement.java
+++ src/main/java/io/java/remove/IMDB_MDS/subscription/SubscriptionManagement.java
@@ -1,14 +1,21 @@
 package io.java.remove.IMDB_MDS.subscription;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.java.remove.IMDB_MDS.model.Subscription;
 import io.java.remove.IMDB_MDS.model.SubscriptionHistory;
 import io.java.remove.IMDB_MDS.model.SubscriptionType;
 import io.java.remove.IMDB_MDS.model.User;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.FileAttribute;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -25,19 +32,19 @@ public class SubscriptionManagement {
     @PostConstruct
     public void init() {
         this.subscriptionHistories = new ArrayList<>();
-        users = loadUsers();
+        users = loadUsersFromStorage();
 
     }
 
 
     private List<User> loadUsers() {
         var basicSubscription = new Subscription(SubscriptionType.BASIC, 10);
-        var proSubscription = new Subscription(SubscriptionType.BASIC, 1000);
-        var premiumSubscription = new Subscription(SubscriptionType.BASIC, Integer.MAX_VALUE);
+        var proSubscription = new Subscription(SubscriptionType.PRO, 1000);
+        var premiumSubscription = new Subscription(SubscriptionType.PREMIUM, Integer.MAX_VALUE);
 
-        var userBasic = new User("Abebe Kebede", "4e53f5ea-4312-4d1e-a010-0f4aab68bceb", basicSubscription);
-        var userPro = new User("Abebe Kebede", "e1b65706-56cb-4bd5-a9b9-159502a05321", proSubscription);
-        var userPremium = new User("Abebe Kebede", "715e5773-a841-462f-92ac-5e758f90d86b", premiumSubscription);
+        var userBasic = new User("Abebe Kebede", "4e53f5ea-4312-4d1e-a010-0f4aab68bceb","ab1@email.com", basicSubscription);
+        var userPro = new User("Abebe Kebede", "e1b65706-56cb-4bd5-a9b9-159502a05321","ab2@email.com", proSubscription);
+        var userPremium = new User("Abebe Kebede", "715e5773-a841-462f-92ac-5e758f90d86b", "ab3@email.com",premiumSubscription);
 
         return List.of(userBasic, userPro, userPremium);
     }
@@ -71,6 +78,53 @@ public class SubscriptionManagement {
     public void subscriptionAudit(SubscriptionHistory subscriptionHistory) {
         log.info("subscription request for user {} , on {}", subscriptionHistory.user().name(), subscriptionHistory.requestTime());
         this.subscriptionHistories.add(subscriptionHistory);
+    }
+
+    private String generateApiKey(){
+        return java.util.UUID.randomUUID().toString();
+    }
+
+    private boolean emailExists(String email){
+        Optional<User> existingUser = users.stream()
+                .filter(user -> user.email().equals(email))
+                .findFirst();
+
+       return existingUser.isPresent();
+    }
+
+    @PreDestroy
+    public void onPreDestroy() {
+        saveToStorage();
+    }
+
+
+
+    private List<User> loadUsersFromStorage() {
+        try {
+            var userStoragePath = Paths.get(System.getProperty("user.home"), "users.json");
+            if (Files.notExists(userStoragePath)) {
+                return new ArrayList<>();
+            }
+            String usersPayloadJson = Files.readString(userStoragePath);
+            return List.of(new ObjectMapper().readValue(usersPayloadJson, User[].class));
+        } catch (IOException e) {
+            log.error("Failed to load users from storage", e);
+            return new ArrayList<>();
+        }
+    }
+
+    private void saveToStorage() {
+        try {
+            var userStoragePath = Paths.get(System.getProperty("user.home"), "users.json");
+            if (Files.notExists(userStoragePath)) {
+                Files.createFile(userStoragePath, new FileAttribute<?>[0]);
+            }
+            String usersPayloadJson = new ObjectMapper().writeValueAsString(users);
+            Files.write(userStoragePath, usersPayloadJson.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+            log.info("Users saved to storage successfully");
+        } catch (IOException e) {
+            log.error("Failed to save users to storage", e);
+        }
     }
 
 
